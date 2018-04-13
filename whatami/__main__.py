a165c7c6b2dde6a8186698684e97cbc27a7939fe
Alex Balderson <alex@menutrinfo.com>
2018-04-13 15:28:54 -0600

    Fix tox errors

--- whatami/__main__.py
+++ whatami/__main__.py
@@ -3,13 +3,20 @@ import argparse
 
 from .discovery import launch
 
+
 def main():
+    """
+    The main method for 'whatami'..
+
+    :return: None
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--json', action='store_true')
     args = parser.parse_args()
 
     launch(args.debug, args.json)
+
 
 if __name__ == '__main__':
     main()
