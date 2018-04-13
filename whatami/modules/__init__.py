a265164af8f796fa50c3ebde472712538484dc3a
Alex Balderson <alex@menutrinfo.com>
2018-04-13 15:13:29 -0600

    remove non existant distros import

--- whatami/modules/__init__.py
+++ whatami/modules/__init__.py
@@ -1,10 +1,10 @@
 """Modules module."""
 from . import clouds
 from . import devices
-from . import distros
+#from . import distros
 
 __all__ = [
     'clouds',
     'devices',
-    'distros'
+    #'distros'
 ]
